88e8e811d5fd12ec64ae3103b2eef13c8951a4b6
Matt Ronchetto <hello@doamatto.xyz>
2022-08-04 07:33:09 -0700

    chore: improved look when failing
    The important things in life.

--- git-passeport.sh
+++ git-passeport.sh
@@ -11,13 +11,16 @@ ALLOWED_REPOS="$@"
 
 IS_ALLOWED=false
 for repo in $ALLOWED_REPOS; do
+    #echo "$repo > $ALLOWED_REPOS : $REPO_PATH" >&2 # uncomment for debugging
     if [[ "$REPO_PATH" == "$repo" ]]; then
         IS_ALLOWED=true
+        break
     fi
 done
 
 if [[ $IS_ALLOWED == "false" ]]; then
-    echo "gitsh: Your SSH key does not have permission to access this repository, or you are trying to access a repository that does not exist." >&2
+    echo "gitsh: Your SSH key does not have permission to access this repository," >&2
+    echo "gitsh: or you are trying to access a repository that does not exist." >&2
     exit 1
 fi
 
