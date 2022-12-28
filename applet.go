b7c39676ff3f987210f2c1dbb22696a71ed8a415
Matthew Ronchetto <hello@doamatto.xyz>
2022-12-28 09:17:07 -0800

    chore: add back repo list functionality and basic error handling

--- applet.go
+++ applet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 )
@@ -17,6 +18,10 @@ type UserData struct {
 func main() {
 	// Fetch user
 	user := os.Getenv("USER")
+	if user == "" {
+		fmt.Println("Missing USER")
+		os.Exit(2)
+	}
 
 	// Read user data from JSON
 	var userData UserData
@@ -31,6 +36,13 @@ func main() {
 
 	// Confirm connecting user has access.
 	repo := os.Getenv("REPO")
+	if repo == "" {
+		fmt.Println("Assuming user wants to see their repos.")
+		repos := getUserRepos(user, userData)
+		fmt.Print(repos)
+		os.Exit(0)
+	}
+
 	repos := getUserRepos(user, userData)
 	for _, r0 := range repos {
 		if r0 == repo {
